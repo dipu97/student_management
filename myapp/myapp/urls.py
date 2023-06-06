dbcf0987b0a3739990961091d83f2bc892330402
Dipu Biswas <dipu@Dipus-MacBook-Pro.local>
2023-06-06 11:17:56 +0600

    base created

---
+++ myapp/myapp/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from .import views, hod_views, staff_views, student_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('base/', views.home, name='home'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
